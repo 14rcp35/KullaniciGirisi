1c742de502427795fa5a5369d77b7cdfcc7ac380
recep ozdes <ultraaslan_417@hotmail.com>
2022-05-17 14:09:31 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String username,password,answer,newPassword1,newPassword2;
+	Scanner input=new Scanner(System.in);
+        System.out.print("Kullanıcı Adınızı Giriniz : ");
+        username=input.nextLine();
+        System.out.print("Şifrenizi Giriniz : ");
+        password=input.nextLine();
+
+        if(username.equals("admin") && password.equals("123")){
+            System.out.println("Giriş Başarılı");
+        }
+        else{
+            System.out.println("Giriş Başarısız");
+            System.out.println("Kullaıcı Adınızı veya Şifrenizi Hatalı Girdiniz Şifre Bilginizi Değiştirmek istermisiniz ? (E/H)");
+            answer=input.nextLine();
+            if(answer.equals("E")||answer.equals("e")){
+                System.out.println("Yeni şifrenizi Giriniz : ");
+                newPassword1=input.nextLine();
+                System.out.println("Yeni şifrenizi Tekrar Giriniz : ");
+                newPassword2=input.nextLine();
+                if(newPassword1.equals(newPassword2)&&username.equals("admin") ){
+                    System.out.println("Şifrenizi Değiştirme İşlemi Başarılı. Kullanıcı Adınız ve Yeni Şifreniz İle Giriş Yapınız");
+                }
+                else{
+                    System.out.println("Şifrenizi Değiştirme İşlemi Başarısız");
+                }
+            }else{
+                System.out.println("Şifre Değiştirme İşlemi Yapılmadı");
+            }
+
+        }
+
+    }
+}
